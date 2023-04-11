9cb8886717c7b6eea17378f7f9a0ad4297f4bc60
Noam Y. Gherson <admin@noamyg.com>
2023-04-11 09:01:42 +0300

    Added fadeIn

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,21 +1,18 @@
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
-import { DialogService, DynamicDialogModule } from 'primeng/dynamicdialog';
+import { DialogService } from 'primeng/dynamicdialog';
 import { DialogModule } from 'primeng/dialog';
-import { ConfirmDialogModule } from 'primeng/confirmdialog';
 import { ReactiveFormsModule } from '@angular/forms';
 import { InputTextModule } from 'primeng/inputtext';
 import { ToastModule } from 'primeng/toast';
-import { MessageService, ConfirmationService } from 'primeng/api';
+import { ConfirmationService } from 'primeng/api';
 import { DropdownModule } from 'primeng/dropdown';
 import { DropdownComponent, InputComponent } from './components/controls';
 import { IndicationTextComponent } from './components/indication-text/indication-text.component';
 import { LoaderComponent } from './components/loader/loader.component';
 import { RippleModule } from 'primeng/ripple';
 import { ButtonModule } from 'primeng/button';
-import { MessagesModule } from 'primeng/messages';
-import { MessageModule } from 'primeng/message';
 import { ConfirmDialogComponent } from './components/confrim-dialog/confirm-dialog.component';
 import { PluckPipe } from './pipes/pluck,pipe';
 
@@ -36,23 +33,18 @@ const sharedModules = [
   CommonModule,
   ReactiveFormsModule,
   HttpClientModule,
-  DynamicDialogModule,
-  ConfirmDialogModule,
   DialogModule,
   ToastModule,
   DropdownModule,
   ButtonModule,
-  RippleModule,
-  MessagesModule,
-  MessageModule
+  RippleModule
 ];
 
 @NgModule({
   declarations,
   providers: [
     DialogService,
-    ConfirmationService,
-    MessageService
+    ConfirmationService
   ],
   imports: [
     ...imports,
